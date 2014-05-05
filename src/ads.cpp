a06c519bc7cb1f2dc3e0b763255f3b38477e3aed
Kevin Koeller <koeller@usc.edu>
2014-05-05 12:58:48 -0700

    started working on ad system without qt, need to fix to work with your msort.h because i need to use lists instead of vectors i think

---
+++ src/ads.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <ostream>
+#include <fstream>
+#include <stdexcept>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <ctype.h>
+#include <stdlib.h>
+#include "msort.h"
+
+using namespace std;
+
+//strut to hold the company and bid details
+struct comp_bid {
+    string keyword;
+    double bid;
+    string company;
+};
+
+//compares two strings
+struct AlphaStrComp {
+        bool operator() (const string& lhs, const string& rhs) 
+        { 
+            if(lhs<=rhs)
+                return true;
+            else
+                return false;
+        }
+};
+
+    //compares integers
+   struct IntComp {
+         bool operator() (const int lhs, const int rhs) 
+        { 
+          if(lhs>rhs)
+                return true;
+            else
+                return false;
+        }
+   };
+
+int main(int argc, char *argv[]){
+    if(argc < 3){
+        cerr << "Need two filenames" << endl;
+        return 1;
+    }
+
+    //input file
+    ifstream ifile(argv[1]);
+    if(ifile.fail()){
+        cerr << "Couldn't open file" << endl;
+        return 1;
+    }
+
+    //output file
+    ofstream ofile(argv[2]);
+    if(ofile.fail()){
+        cerr << "Couldn't open file" << endl;
+        return 1;
+    }
+
+    string key;
+    cout<<"Enter keyword"<<endl;
+    cin>>key;
+
+    vector<comp_bid> comp;
+    comp_bid cb;  
+    vector<double> bidVec;
+    vector<string> compTempVec, compVec; 
+    IntComp c;
+    AlphaStrComp c2;
+
+
+    int count=0;
+    int numLines;
+    int j=0;
+
+    //reads the file line by line, extracts every word and stores it in the struct object. Then push the struct obj into the vector comp
+    while (getline(ifile, line))  //while their are lines on the page
+    {
+        stringstream ss;
+        ss << line;
+        if(count==0) {
+            ss>>numLines;
+        }
+        else if(count>0 && count <=numLines) { 
+            ss>>cb.keyword;
+            ss>>cb.bid;
+            ss>>cb.company;
+            comp.push_back(cb);
+        }
+
+        count ++;
+    }
+
+
+     ifile.close();
+
+
+     //if the keyword in the struct matches the user entered keyword store the bid value in bidvec
+
+    for(int i=0;i<numLines;i++) {
+        cb = comp[i];
+        if(cb.keyword == key) {
+            bidVec[i]=cb.bid;
+        }
+    }   
+      
+
+    //sort all the bidvalues
+     for(int i=0;i<numLines;i++) {
+     merge_sort(bidVec, c);
+    }
+
+     //if the keyword in the struct matches the user entered keyword and bid value equals the one in the vector store the company name for that bid value in a temp vactor    
+    for(int j=0; j<bidVec.size(); j++) {
+     for(int i=0;i<numLines;i++) {
+        cb = comp[i];
+        if(cb.keyword == key && cb.bid == bidArr[j]) {
+            compTempVec.push_back(cb.company);
+        }
+    }  
+
+    //sort the temp vector alphabetically (company names) 
+    merge_sort(compTempVec, c2);
+    //push all the company names into a vector
+    compVec.push_back(cb.company);
+    }
+
+    //print these company names
+    for(int i=0;i<compVec.size();i++) {
+       cout<<compVec[i];
+    }
